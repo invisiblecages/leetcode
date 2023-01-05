d5898f01438d9c4fb7578344904f5167b0a89e02
Wenzel <70312879+invisiblecages@users.noreply.github.com>
2023-01-05 18:30:09 +0100

    added new problems

---
+++ algorithms/0290. Word Pattern/Program.cs
@@ -0,0 +1,82 @@
+﻿/*
+Given a pattern and a string s, find if s follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+
+Example 1:
+Input: pattern = "abba", s = "dog cat cat dog"
+Output: true
+
+Example 2:
+Input: pattern = "abba", s = "dog cat cat fish"
+Output: false
+
+Example 3:
+Input: pattern = "aaaa", s = "dog cat cat dog"
+Output: false
+
+Constraints:
+    1 <= pattern.length <= 300
+    pattern contains only lower-case English letters.
+    1 <= s.length <= 3000
+    s contains only lowercase English letters and spaces ' '.
+    s does not contain any leading or trailing spaces.
+    All the words in s are separated by a single space.
+*/
+// Slow first solution
+// Time complexity: O(n)
+// Space complexity: O(1)
+public class Solution
+{
+  public bool WordPattern(string pattern, string s)
+  {
+    string[] arr = s.Split(' ');
+    if (pattern.Length != arr.Length)
+        return false;
+	
+	//Dictionary<char, string> dict = new() is much slower
+    Dictionary<string, string> dict = new();
+    for (int i = 0; i < pattern.Length; i++)
+    {
+      string key = pattern[i].ToString();
+      string str = arr[i];
+      if (!dict.ContainsKey(key) && !dict.ContainsValue(str))
+        dict[key] = str;
+      else if (!dict.ContainsKey(key) && dict.ContainsValue(str))
+        return false;
+      else if (dict.TryGetValue(key, out string? value))
+        if (value != str)
+          return false;
+    }
+    
+    return true;
+  }
+}
+
+// faster and cleaner code
+public bool WordPattern1(string pattern, string s)
+{
+    string[] arr = s.Split(' ');
+    if (pattern.Length != arr.Length)
+        return false;
+    
+    Dictionary<string, string> dict = new();
+    for (int i = 0; i < pattern.Length; i++)
+    {
+        string key = pattern[i].ToString();
+        if (dict.ContainsKey(key))
+        {
+            if (dict[key] != arr[i])
+                return false;
+        }
+        else
+        {
+            if (dict.ContainsValue(arr[i]))
+                return false;
+
+            dict[key] = arr[i];
+        }
+    }
+
+    return true;
+}
---
+++ algorithms/1189. Maximum Number of Balloons/Program.cs
@@ -0,0 +1,87 @@
+﻿/*
+Given a string text, you want to use the characters of text to form as many instances of the word "balloon" as possible.
+
+You can use each character in text at most once. Return the maximum number of instances that can be formed.
+
+Example 1:
+Input: text = "nlaebolko"
+Output: 1
+
+Example 2:
+Input: text = "loonbalxballpoon"
+Output: 2
+
+Example 3:
+Input: text = "leetcode"
+Output: 0
+
+Constraints:
+    1 <= text.length <= 104
+    text consists of lower case English letters only.
+*/
+// Time complexity: O(n)
+// Space complexity: O(1)
+public class Solution
+{
+  public int MaxNumberOfBalloons(string text)
+  {
+    Dictionary<char, int> dict = new()
+    {
+      { 'b', 0 },
+      { 'a', 0 },
+      { 'l', 0 },
+      { 'o', 0 },
+      { 'n', 0 }
+    };
+
+    foreach (char c in text)
+      if (dict.ContainsKey(c))
+        dict[c]++;
+    
+    int cnt = 0;
+    while (dict['b'] > 0 && dict['a'] > 0 && dict['l'] > 1 && dict['o'] > 1 && dict['n'] > 0)
+    {
+        dict['b']--;
+        dict['a']--;
+        dict['l'] -= 2;
+        dict['o'] -= 2;
+        dict['n']--;
+        cnt++;
+    }
+
+    return cnt;
+  }
+}
+// General solution which is a bit slower
+// Time complexity: O(n)
+// Space complexity: O(1)
+public class Solution
+{
+  public int MaxNumberOfBalloons(string text)
+  {
+    Dictionary<char, int> textDict = new();
+    Dictionary<char, int> wordDict = new();
+    string word = "balloon";
+    
+    foreach (char c in word)
+      if (wordDict.ContainsKey(c))
+          wordDict[c]++;
+      else
+          wordDict[c] = 1;
+
+    foreach (char c in text)
+      if (wordDict.ContainsKey(c))
+      {
+        if (textDict.ContainsKey(c))
+            textDict[c]++;
+        else
+            textDict[c] = 1;
+      }
+
+    int cnt = text.Length;
+    foreach (char c in wordDict.Keys)
+      cnt = Math.Min(cnt, textDict.GetValueOrDefault(c, 0) / wordDict[c]);
+
+    return cnt;
+  }
+}
