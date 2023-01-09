f75744078a5a135dc5e6470be53bcf5e59aa945d
Wenzel <70312879+invisiblecages@users.noreply.github.com>
2023-01-09 22:26:02 +0100

    added solved problems

---
+++ algorithms/0021. Merge Two Sorted Lists/Program.cs
@@ -0,0 +1,88 @@
+﻿/*
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+Example 1:
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Example 2:
+Input: list1 = [], list2 = []
+Output: []
+
+Example 3:
+Input: list1 = [], list2 = [0]
+Output: [0]
+
+Constraints:
+    The number of nodes in both lists is in the range [0, 50].
+    -100 <= Node.val <= 100
+    Both list1 and list2 are sorted in non-decreasing order.
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+// Time complexity: O(n)
+// Space complexity: O(n)
+public class Solution
+{
+  public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+  {
+    ListNode list3 = new();
+    ListNode tail = list3;
+    while (list1 != null && list2 != null)
+    {
+      if (list1.val > list2.val)
+      {
+        list3.next = list2;
+        list2 = list2.next;
+      }
+      else
+      {
+        list3.next = list1;
+        list1 = list1.next;
+      }
+
+      list3 = list3.next;
+    }
+    
+    if (list1 != null)
+      list3.next = list1;
+    else
+      list3.next = list2;
+
+    return tail.next;
+  }
+}
+// Recursion
+public class Solution {
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null)
+            return list2;
+
+        if(list2 == null)
+            return list1;
+
+        if(list1.val < list2.val)
+        {
+            list1.next = MergeTwoLists(list1.next, list2);
+            return list1;
+        }
+        else
+        {
+            list2.next = MergeTwoLists(list2.next, list1);
+            return list2;
+        }
+    }
+}
---
+++ algorithms/0144. Binary Tree Preorder Traversal/Program.cs
@@ -0,0 +1,80 @@
+﻿/*
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+
+Example 2:
+Input: root = []
+Output: []
+
+Example 3:
+Input: root = [1]
+Output: [1]
+
+Constraints:
+    The number of nodes in the tree is in the range [0, 100].
+    -100 <= Node.val <= 100
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+*/
+// Time complexity: O(n)
+// Space complexity: O(n)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+  public IList<int> PreorderTraversal(TreeNode root)
+  {
+    List<int> res = new();
+    if (root == null)
+      return res;
+
+    Stack<TreeNode> stack = new();
+    stack.Push(root);
+    while (stack.Count > 0)
+    {
+      TreeNode curr = stack.Pop();
+      res.Add(curr.val);
+
+      if (curr.right != null)
+        stack.Push(curr.right);
+      if (curr.left != null)
+        stack.Push(curr.left);
+    }
+
+    return res;
+  }
+}
+// Recursive
+// Time complexity: O(n)
+// Space complexity: O(n)
+public class Solution
+{
+    public IList<int> PreorderTraversal(TreeNode root)
+    {
+        var list = new List<int>();
+        PreorderTraversalRecursive(root, list);
+        return list;
+    }
+
+    private void PreorderTraversalRecursive(TreeNode node, List<int> list)
+    {
+        if (node == null) return;
+        list.Add(node.val);
+        PreorderTraversalRecursive(node.left, list);
+        PreorderTraversalRecursive(node.right, list);
+    }
+}
