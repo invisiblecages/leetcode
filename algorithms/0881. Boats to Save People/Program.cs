0f5e8d5787c38c24cb784f23c9ff340761ec564e
Wenzel <70312879+invisiblecages@users.noreply.github.com>
2023-04-03 21:26:27 +0200

    added solution

---
+++ algorithms/0881. Boats to Save People/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+You are given an array people where people[i] is the weight of the ith person, 
+and an infinite number of boats where each boat can carry a maximum weight of limit. 
+Each boat carries at most two people at the same time, provided the sum of the weight of those people is at most limit.
+
+Return the minimum number of boats to carry every given person.
+
+Example 1:
+Input: people = [1,2], limit = 3
+Output: 1
+Explanation: 1 boat (1, 2)
+
+Example 2:
+Input: people = [3,2,2,1], limit = 3
+Output: 3
+Explanation: 3 boats (1, 2), (2) and (3)
+
+Example 3:
+Input: people = [3,5,3,4], limit = 5
+Output: 4
+Explanation: 4 boats (3), (3), (4), (5)
+
+Constraints:
+    1 <= people.length <= 5 * 104
+    1 <= people[i] <= limit <= 3 * 104
+*/
+// Time complexity: O(n log n)
+// Space complexity: O(1)
+public class Solution 
+{
+  public int NumRescueBoats(int[] people, int limit) 
+  {
+    int boats = 0;
+    int left = 0;
+    int right = people.Length - 1;
+    Array.Sort(people);
+
+    while (left <= right)
+    {
+      if (people[left] + people[right] <= limit)
+      {
+        boats++;
+        left++;
+        right--;
+      }
+      else if (people[right] <= limit)
+      {
+        boats++;
+        right--;
+      }
+      else 
+      {
+        boats++;
+        right--;
+      }
+    }
+    
+    return boats;
+  }
+}
