282cf81cfac5d1ce7d9a98f17dfd34f203189d3b
Wenzel <70312879+invisiblecages@users.noreply.github.com>
2023-04-09 21:14:52 +0200

    added solutions

---
+++ database/0182. Duplicate Emails.sql
@@ -0,0 +1,49 @@
+﻿/* Description */
+Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+
+Write an SQL query to report all the duplicate emails. Note that its guaranteed that the email field is not NULL.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+Example 1:
+
+Input: 
+Person table:
++----+---------+
+| id | email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+Output: 
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+
+Explanation: a@b.com is repeated two times.
+
+/* SQL Schema */
+Create table If Not Exists Person (id int, email varchar(255))
+Truncate table Person
+insert into Person (id, email) values ('1', 'a@b.com')
+insert into Person (id, email) values ('2', 'c@d.com')
+insert into Person (id, email) values ('3', 'a@b.com')
+
+/* Write your T-SQL query statement below */
+SELECT email
+FROM Person
+GROUP BY email
+HAVING COUNT(email) > 1
