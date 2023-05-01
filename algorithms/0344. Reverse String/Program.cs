dd56cb523181100c068217172bff4a3440bea2ce
Wenzel <70312879+invisiblecages@users.noreply.github.com>
2023-05-01 21:28:51 +0200

    added solution

---
+++ algorithms/0344. Reverse String/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+
+Constraints:
+
+    1 <= s.length <= 105
+    s[i] is a printable ascii character.
+*/
+// Time complexity: O(n)
+// Space complexity: O(1)
+public class Solution 
+{
+  public void ReverseString(char[] s) 
+  {
+    int left = 0;
+    int right = s.Length - 1;
+    
+    while (left < right)
+    {
+      char move = s[left];
+      s[left] = s[right];
+      s[right] = move;
+      left++;
+      right--;
+    }     
+  }
+}
