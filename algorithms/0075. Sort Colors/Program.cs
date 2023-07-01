9762cffdd51cc5b94b7055c4b1015554e9217560
Wenzel <70312879+invisiblecages@users.noreply.github.com>
2023-07-01 16:02:27 +0200

    added solution

---
+++ algorithms/0075. Sort Colors/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, 
+with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+
+Constraints:
+
+    n == nums.length
+    1 <= n <= 300
+    nums[i] is either 0, 1, or 2.
+
+Follow up: Could you come up with a one-pass algorithm using only constant extra space?
+*/
+// Time complexity: O(n)
+// Space complexity: O(1)
+public class Solution 
+{
+  private void SwapNumbers(int i, int j, int[] array)
+  {
+    int temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+
+  public void SortColors(int[] nums) 
+  {
+    int left = 0;
+    int right = nums.Length - 1;
+    int index = 0;
+
+    while (index <= right)
+    {
+      if (nums[index] == 0)
+        SwapNumbers(index++, left++, nums);
+      else if (nums[index] == 2)
+        SwapNumbers(index, right--, nums);
+      else 
+        index++;
+    }
+  }
+}
