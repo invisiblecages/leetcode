2499665983871a28a14fee091fc568e77dd923bf
Wenzel <70312879+invisiblecages@users.noreply.github.com>
2023-08-11 09:51:27 +0200

    added solution

---
+++ algorithms/0852. Peak Index in a Mountain Array/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+An array arr a mountain if the following properties hold:
+
+    arr.length >= 3
+    There exists some i with 0 < i < arr.length - 1 such that:
+        arr[0] < arr[1] < ... < arr[i - 1] < arr[i] 
+        arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+
+Given a mountain array arr, return the index i such that 
+arr[0] < arr[1] < ... < arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1].
+
+You must solve it in O(log(arr.length)) time complexity.
+
+Example 1:
+
+Input: arr = [0,1,0]
+Output: 1
+
+Example 2:
+
+Input: arr = [0,2,1,0]
+Output: 1
+
+Example 3:
+
+Input: arr = [0,10,5,2]
+Output: 1
+
+Constraints:
+
+    3 <= arr.length <= 105
+    0 <= arr[i] <= 106
+    arr is guaranteed to be a mountain array.
+*/
+// Time complexity: O(log n)
+// Space complexity: O(1)
+public class Solution 
+{
+  public int PeakIndexInMountainArray(int[] arr) 
+  {
+    int left = 0;
+    int right = arr.Length - 1;
+
+    while (left <= right)
+    {
+      int mid = left + (right - left) / 2;
+
+      if (arr[mid] < arr[mid + 1])
+        left = mid + 1;
+      else
+        right = mid - 1;
+    }
+
+    return left;
+  }
+}
