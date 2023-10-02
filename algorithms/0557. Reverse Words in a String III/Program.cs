348b0cd3d004650e734f7037152036aab0d10ac9
Wenzel <70312879+invisiblecages@users.noreply.github.com>
2023-10-02 09:15:56 +0200

    added solution

---
+++ algorithms/0557. Reverse Words in a String III/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+Example 1:
+
+Input: s = "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+
+Example 2:
+
+Input: s = "God Ding"
+Output: "doG gniD"
+
+Constraints:
+
+    1 <= s.length <= 5 * 104
+    s contains printable ASCII characters.
+    s does not contain any leading or trailing spaces.
+    There is at least one word in s.
+    All the words in s are separated by a single space.
+*/
+// Time complexity: O(n)
+// Space complexity: O(n)
+public class Solution 
+{
+  public string ReverseWords(string s) 
+  {
+    string[] words = s.Split();
+
+    for (int i = 0; i < words.Length; i++)
+    {
+      string word = "";
+
+      for (int j = words[i].Length - 1; j >= 0; j--)
+        word += words[i][j];
+      
+      words[i] = word;
+    }
+
+    return string.Join(" ", words);
+  }
+}
