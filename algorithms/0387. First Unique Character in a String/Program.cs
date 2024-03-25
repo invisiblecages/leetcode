ab22fea42e9b32539ef44793ab0e4299fc86351d
Wenzel <70312879+invisiblecages@users.noreply.github.com>
2024-03-25 18:28:39 +0100

    added solutions

---
+++ algorithms/0387. First Unique Character in a String/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+Example 1:
+
+Input: s = "leetcode"
+Output: 0
+
+Example 2:
+
+Input: s = "loveleetcode"
+Output: 2
+
+Example 3:
+
+Input: s = "aabb"
+Output: -1
+
+Constraints:
+
+    1 <= s.length <= 10^5
+    s consists of only lowercase English letters.
+*/
+// Time complexity: O(n)
+// Space complexity: O(n)
+public class Solution 
+{
+  public int FirstUniqChar(string s) 
+  {
+    Dictionary<char, List<int>> chars = [];
+
+    for (int i = 0; i < s.Length; i++)
+    {
+      char c = s[i];
+
+      if (chars.ContainsKey(c))
+      {
+        chars[c].Add(i);
+      }
+      else
+      {
+        chars[c] = [i];
+      }
+    }
+
+    foreach (var list in chars.Values)
+    {
+      if (list.Count == 1)
+      {
+        return list[0];
+      }
+    }
+
+    return -1;  
+  }
+}
---
+++ algorithms/0442. Find All Duplicates in an Array/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears once or twice, 
+return an array of all the integers that appears twice.
+
+You must write an algorithm that runs in O(n) time and uses only constant extra space.
+
+Example 1:
+
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [2,3]
+
+Example 2:
+
+Input: nums = [1,1,2]
+Output: [1]
+
+Example 3:
+
+Input: nums = [1]
+Output: []
+
+Constraints:
+
+    n == nums.length
+    1 <= n <= 10^5
+    1 <= nums[i] <= n
+    Each element in nums appears once or twice.
+*/
+// Time complexity: O(n)
+// Space complexity: O(1)
+public class Solution 
+{
+  public IList<int> FindDuplicates(int[] nums) 
+  {
+    List<int> dupes = [];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+      int num = Math.Abs(nums[i]);
+
+      if (nums[num - 1] < 0)
+      {
+        dupes.Add(num);
+      } 
+      else
+      {
+        nums[num - 1] *= -1;
+      }
+    }
+
+    return dupes;
+  }
+}
