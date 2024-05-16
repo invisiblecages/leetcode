4ea432e4465cb4ae535b027a8864c18e4450a5c9
Wenzel <70312879+invisiblecages@users.noreply.github.com>
2024-05-16 22:00:01 +0200

    added solution

---
+++ algorithms/2331. Evaluate Boolean Binary Tree/Program.cs
@@ -0,0 +1,91 @@
+﻿/*
+You are given the root of a full binary tree with the following properties:
+
+    Leaf nodes have either the value 0 or 1, where 0 represents False and 1 represents True.
+    Non-leaf nodes have either the value 2 or 3, where 2 represents the boolean OR and 3 represents the boolean AND.
+
+The evaluation of a node is as follows:
+
+    If the node is a leaf node, the evaluation is the value of the node, i.e. True or False.
+    Otherwise, evaluate the node's two children and apply the boolean operation of its value with the children's evaluations.
+
+Return the boolean result of evaluating the root node.
+
+A full binary tree is a binary tree where each node has either 0 or 2 children.
+
+A leaf node is a node that has zero children.
+
+Example 1:
+
+Input: root = [2,1,3,null,null,0,1]
+Output: true
+Explanation: The above diagram illustrates the evaluation process.
+The AND node evaluates to False AND True = False.
+The OR node evaluates to True OR False = True.
+The root node evaluates to True, so we return true.
+
+Example 2:
+
+Input: root = [0]
+Output: false
+Explanation: The root node is a leaf node and it evaluates to false, so we return false.
+
+Constraints:
+
+    The number of nodes in the tree is in the range [1, 1000].
+    0 <= Node.val <= 3
+    Every node has either 0 or 2 children.
+    Leaf nodes have a value of 0 or 1.
+    Non-leaf nodes have a value of 2 or 3.
+*/
+// Time complexity: O(n)
+// Space complexity: O(n)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution 
+{
+  /* 
+  0 = false
+  1 = true
+  2 = ||
+  3 = &&
+  */
+  private bool DFS(TreeNode node)
+  {
+    if (node is null)
+    {
+      return false;
+    }
+
+    bool left = DFS(node.left);
+    bool right = DFS(node.right);
+
+    if (node.val == 2)
+    {
+      return left || right;
+    }
+
+    if (node.val == 3)
+    {
+      return left && right;
+    }
+
+    return Convert.ToBoolean(node.val);
+  }
+
+  public bool EvaluateTree(TreeNode root) 
+  {
+    return DFS(root);
+  }
+}
