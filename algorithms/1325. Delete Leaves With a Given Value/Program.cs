ec6b7aeb997ec5a4b93b01432b77a04f44d648da
Wenzel <70312879+invisiblecages@users.noreply.github.com>
2024-05-17 10:53:32 +0200

    added solution

---
+++ algorithms/1325. Delete Leaves With a Given Value/Program.cs
@@ -0,0 +1,69 @@
+﻿/*
+Given a binary tree root and an integer target, delete all the leaf nodes with value target.
+
+Note that once you delete a leaf node with value target, if its parent node becomes a leaf node and has the value target, 
+it should also be deleted (you need to continue doing that until you cannot).
+
+Example 1:
+
+Input: root = [1,2,3,2,null,2,4], target = 2
+Output: [1,null,3,null,4]
+Explanation: Leaf nodes in green with value (target = 2) are removed (Picture in left). 
+After removing, new nodes become leaf nodes with value (target = 2) (Picture in center).
+
+Example 2:
+
+Input: root = [1,3,3,3,2], target = 3
+Output: [1,3,null,null,2]
+
+Example 3:
+
+Input: root = [1,2,null,2,null,2], target = 2
+Output: [1]
+Explanation: Leaf nodes in green with value (target = 2) are removed at each step.
+
+Constraints:
+
+    The number of nodes in the tree is in the range [1, 3000].
+    1 <= Node.val, target <= 1000
+*/
+// Time complexity: O(n)
+// Space complexity: O(n)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution 
+{
+  private TreeNode DFS(TreeNode node, int target)
+  {
+    if (node is null)
+    {
+      return null;
+    }
+
+    node.left = DFS(node.left, target);
+    node.right = DFS(node.right, target);
+
+    if (node.left is null && node.right is null && node.val == target)
+    {
+      return null;
+    }
+
+    return node;
+  }
+
+  public TreeNode RemoveLeafNodes(TreeNode root, int target) 
+  {
+    return DFS(root, target);
+  }
+}
