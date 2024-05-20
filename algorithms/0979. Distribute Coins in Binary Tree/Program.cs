11fcaf4728bd7b10353290ccc0175199b82606c6
Wenzel <70312879+invisiblecages@users.noreply.github.com>
2024-05-20 23:34:48 +0200

    added solution

---
+++ algorithms/0979. Distribute Coins in Binary Tree/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+You are given the root of a binary tree with n nodes where each node in the tree has node.val coins. 
+There are n coins in total throughout the whole tree.
+
+In one move, we may choose two adjacent nodes and move one coin from one node to another. 
+A move may be from parent to child, or from child to parent.
+
+Return the minimum number of moves required to make every node have exactly one coin.
+
+Example 1:
+
+Input: root = [3,0,0]
+Output: 2
+Explanation: From the root of the tree, we move one coin to its left child, and one coin to its right child.
+
+Example 2:
+
+Input: root = [0,3,0]
+Output: 3
+Explanation: From the left child of the root, we move two coins to the root [taking two moves]. 
+Then, we move one coin from the root of the tree to the right child.
+
+Constraints:
+
+    The number of nodes in the tree is n.
+    1 <= n <= 100
+    0 <= Node.val <= n
+    The sum of all Node.val is n.
+*/
+// Time complexity: O(n)
+// Space complexity: O(n)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution 
+{
+  private (int coins, int moves) DFS(TreeNode node)
+  {
+    if (node is null)
+    {
+      return (0, 0);
+    }
+
+    var left = DFS(node.left);
+    var right = DFS(node.right);
+
+    var coins = node.val - 1 + left.coins + right.coins;
+    var moves = left.moves + right.moves + Math.Abs(left.coins) + Math.Abs(right.coins);
+    
+    return (coins, moves);
+  }
+
+  public int DistributeCoins(TreeNode root) 
+  {
+    return DFS(root).moves;
+  }
+}
