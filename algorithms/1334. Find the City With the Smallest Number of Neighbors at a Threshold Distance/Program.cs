d98cf0b229db117f9ab3c14b0cd6beadfdc80694
Wenzel <70312879+invisiblecages@users.noreply.github.com>
2024-07-29 10:50:57 +0200

    added solutions

---
+++ algorithms/1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance/Program.cs
@@ -0,0 +1,92 @@
+﻿// Time complexity: O(n^3)
+// Space complexity: O(n^2)
+public class Solution 
+{
+  public class Graph
+  {
+    private int[,] _matrix;
+    private int _n;
+
+    public Graph(int n, int[][] edges)
+    {
+      _n = n;
+      InitializeMatrix();
+      AddEdges(edges);
+    }
+
+    private void InitializeMatrix()
+    {
+      _matrix = new int[_n, _n];
+
+      for (int i = 0; i < _n; i++) 
+      {
+        for (int j = 0; j < _n; j++) 
+        {
+          _matrix[i, j] = (i == j) ? 0 : 10001;
+        }
+      }
+    }
+
+    private void AddEdges(int[][] edges)
+    {
+      foreach (var edge in edges)
+      {
+        int from = edge[0];
+        int to = edge[1];
+        int weight = edge[2];
+
+        _matrix[from, to] = weight;
+        _matrix[to, from] = weight;
+      }
+    }
+
+    public void FloydWarshall()
+    {
+      for (int k = 0; k < _n; k++) 
+      {
+        for (int i = 0; i < _n; i++) 
+        {
+          for (int j = 0; j < _n; j++) 
+          {
+            _matrix[i, j] = Math.Min(_matrix[i, j], _matrix[i, k] + _matrix[k, j]);
+          }
+        }
+      }
+    }
+
+    public int FindNodeWithMinNeighbors(int distanceThreshold)
+    {
+      int node = 0;
+      int minNeighbors = _n;
+
+      for (int i = 0; i < _n; i++)
+      {
+        int neighbors = 0;
+
+        for (int j = 0; j < _n; j++)
+        {
+          if (_matrix[i, j] <= distanceThreshold)
+          {
+            neighbors++;
+          }
+        }
+
+        if (neighbors <= minNeighbors)
+        {
+          minNeighbors = neighbors;
+          node = i;
+        }
+      }
+
+      return node;
+    }
+  }
+
+  public int FindTheCity(int n, int[][] edges, int distanceThreshold) 
+  {
+    var graph = new Graph(n, edges);
+    graph.FloydWarshall();
+
+    return graph.FindNodeWithMinNeighbors(distanceThreshold);
+  }
+}
---
+++ algorithms/2976. Minimum Cost to Convert String I/Program.cs
@@ -0,0 +1,82 @@
+﻿// Time complexity: O(n)
+// Space complexity: O(1)
+public class Solution 
+{
+  public class Graph
+  {
+    private const int IMPOSSIBLE = 1000001 * 2;
+    private const int MAXNODES = 26;
+    private int[,] _matrix;
+
+    public Graph(char[] original, char[] changed, int[] cost)
+    {
+      _matrix = new int[MAXNODES, MAXNODES];
+      InitializeMatrix();
+      AddEdges(original, changed, cost);
+    }
+
+    private void InitializeMatrix()
+    {
+      for (int i = 0; i < MAXNODES; i++) 
+      {
+        for (int j = 0; j < MAXNODES; j++) 
+        {
+          _matrix[i, j] = (i == j) ? 0 : IMPOSSIBLE;
+        }
+      }
+    }
+
+    private void AddEdges(char[] original, char[] changed, int[] cost)
+    {
+      for (int i = 0; i < original.Length; i++)
+      {
+        int from = original[i] - 'a';
+        int to = changed[i] - 'a';
+
+        _matrix[from, to] =  Math.Min(_matrix[from, to], cost[i]);
+      }
+    }
+
+    public void FloydWarshall()
+    {
+      for (int k = 0; k < MAXNODES; k++) 
+      {
+        for (int i = 0; i < MAXNODES; i++) 
+        {
+          for (int j = 0; j < MAXNODES; j++) 
+          {
+            _matrix[i, j] = Math.Min(_matrix[i, j], _matrix[i, k] + _matrix[k, j]);
+          }
+        }
+      }
+    }
+
+    public long GetMinimumCostToConvertString(string source, string target)
+    {
+      long cost = 0;
+
+      for (int i = 0; i < source.Length; i++)
+      {
+        int src = source[i] - 'a';
+        int tar = target[i] - 'a';
+
+        if (_matrix[src, tar] == IMPOSSIBLE)
+        {
+          return -1;
+        }
+
+        cost += _matrix[src, tar];
+      }
+
+      return cost;
+    }
+  }
+
+  public long MinimumCost(string source, string target, char[] original, char[] changed, int[] cost) 
+  {
+    var graph = new Graph(original, changed, cost);
+    graph.FloydWarshall();
+
+    return graph.GetMinimumCostToConvertString(source, target);
+  }
+}
